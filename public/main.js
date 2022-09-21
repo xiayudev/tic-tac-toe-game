02129f3905c3ec66fe6d52a7e7e732dd88accdcd
joslab <jos.ct101@gmail.com>
2022-09-21 16:24:01 -0500

    Mejorando el archivo main.js
    Signed-off-by: joslab <jos.ct101@gmail.com>

--- public/main.js
+++ public/main.js
@@ -38,30 +38,31 @@ function drawMatrix() {
 }
 
 function colocarXO(casilla) {
+    const $cuadro = document.getElementById(casilla);
     if (nextTurn === 1) {
-        document.getElementById(casilla).disabled = true;
-        document.getElementById(casilla).innerText = player1Move;
-        document.getElementById(casilla).style.background = '#16213E';
-        document.getElementById(casilla).style.color = '#777';
-        document.getElementById(casilla).style.cursor = 'default';
-        document.getElementById(casilla).style.border = '2px solid #444';
+        $cuadro.disabled = true;
+        $cuadro.innerText = player1Move;
+        $cuadro.style.background = '#16213E';
+        $cuadro.style.color = '#777';
+        $cuadro.style.cursor = 'default';
+        $cuadro.style.border = '2px solid #444';
         document.getElementById('turn').innerText = 'Turno del jugador 2';
         nextTurn = player2;
 
     } else {
-        document.getElementById(casilla).disabled = true;
-        document.getElementById(casilla).innerText = player2Move;
-        document.getElementById(casilla).style.background = '#16213E';
-        document.getElementById(casilla).style.color = '#777';
-        document.getElementById(casilla).style.cursor = 'default';
-        document.getElementById(casilla).style.border = '2px solid #444';
+        $cuadro.disabled = true;
+        $cuadro.innerText = player2Move;
+        $cuadro.style.background = '#16213E';
+        $cuadro.style.color = '#777';
+        $cuadro.style.cursor = 'default';
+        $cuadro.style.border = '2px solid #444';
         document.getElementById('turn').innerText = 'Turno del jugador 1';
         nextTurn = player1;
     }
     checkWinner();
 }
 
-function winner({ boton, btn1, btn2, btn3, jugador }) {
+function message({ boton, btn1, btn2, btn3, jugador }) {
     Swal.fire({
         title: '<strong style="font-family: Kalam, cursive; color: #C3FF99;">HAY UN GANADOR</strong>',
         html: '<span style="font-family: Kalam, cursive;">¡El jugador ' + jugador + ' ha ganado!</span>',
@@ -103,173 +104,86 @@ function winner({ boton, btn1, btn2, btn3, jugador }) {
     $turn.innerText = '';
 }
 
-function checkWinner() {
-    // llamamos a todos los botones creados
-    const $btn1 = document.getElementById('1');
-    const $btn2 = document.getElementById('2');
-    const $btn3 = document.getElementById('3');
-    const $btn4 = document.getElementById('4');
-    const $btn5 = document.getElementById('5');
-    const $btn6 = document.getElementById('6');
-    const $btn7 = document.getElementById('7');
-    const $btn8 = document.getElementById('8');
-    const $btn9 = document.getElementById('9');
-    const $boton = document.getElementsByClassName('boton');
-
-
+function winner({ botones, boton, playerMove, playerTurn }) {
     /** ### X ### */
     // Verificar en cada fila si existen 3 'X' consecutivos
-    if (($btn1.textContent === player1Move) && ($btn2.textContent === player1Move) && ($btn3.textContent === player1Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn1,
-            btn2: $btn2,
-            btn3: $btn3,
-            jugador: 1
+    if ((botones.one.textContent === playerMove) && (botones.two.textContent === playerMove) && (botones.three.textContent === playerMove)) {
+        message({
+            boton,
+            btn1: botones.one,
+            btn2: botones.two,
+            btn3: botones.three,
+            jugador: playerTurn
         });
-    } else if (($btn4.textContent === player1Move) && ($btn5.textContent === player1Move) && ($btn6.textContent === player1Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn4,
-            btn2: $btn5,
-            btn3: $btn6,
-            jugador: 1
-        });
-
-    } else if (($btn7.textContent === player1Move) && ($btn8.textContent === player1Move) && ($btn9.textContent === player1Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn7,
-            btn2: $btn8,
-            btn3: $btn9,
-            jugador: 1
+    } else if ((botones.four.textContent === playerMove) && (botones.five.textContent === playerMove) && (botones.six.textContent === playerMove)) {
+        message({
+            boton,
+            btn1: botones.four,
+            btn2: botones.five,
+            btn3: botones.six,
+            jugador: playerTurn
         });
 
-        // Verificar en cada columna si existen 3 'X' consecutivos
-    } else if (($btn1.textContent === player1Move) && ($btn4.textContent === player1Move) && ($btn7.textContent === player1Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn1,
-            btn2: $btn4,
-            btn3: $btn7,
-            jugador: 1
+    } else if ((botones.seven.textContent === playerMove) && (botones.eigth.textContent === playerMove) && (botones.nine.textContent === playerMove)) {
+        message({
+            boton,
+            btn1: botones.seven,
+            btn2: botones.eigth,
+            btn3: botones.nine,
+            jugador: playerTurn
         });
 
-    } else if (($btn2.textContent === player1Move) && ($btn5.textContent === player1Move) && ($btn8.textContent === player1Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn2,
-            btn2: $btn5,
-            btn3: $btn8,
-            jugador: 1
+        //Verificar en cada columna si existen 3 'X' consecutivos
+    } else if ((botones.one.textContent === playerMove) && (botones.four.textContent === playerMove) && (botones.seven.textContent === playerMove)) {
+        message({
+            boton,
+            btn1: botones.one,
+            btn2: botones.four,
+            btn3: botones.seven,
+            jugador: playerTurn
         });
 
-    } else if (($btn3.textContent === player1Move) && ($btn6.textContent === player1Move) && ($btn9.textContent === player1Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn3,
-            btn2: $btn6,
-            btn3: $btn9,
-            jugador: 1
+    } else if ((botones.two.textContent === playerMove) && (botones.five.textContent === playerMove) && (botones.eigth.textContent === playerMove)) {
+        message({
+            boton,
+            btn1: botones.two,
+            btn2: botones.five,
+            btn3: botones.eigth,
+            jugador: playerTurn
         });
 
-        // Verificar en la diagonal si existen 3 'X' consecutivos
-    } else if (($btn1.textContent === player1Move) && ($btn5.textContent === player1Move) && ($btn9.textContent === player1Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn1,
-            btn2: $btn5,
-            btn3: $btn9,
-            jugador: 1
-        });
-
-    } else if (($btn3.textContent === player1Move) && ($btn5.textContent === player1Move) && ($btn7.textContent === player1Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn3,
-            btn2: $btn5,
-            btn3: $btn7,
-            jugador: 1
-        });
-
-        /** ### O ### */
-        // Verificar en cada fila si existen 3 'X' consecutivos
-    } else if (($btn1.textContent === player2Move) && ($btn2.textContent === player2Move) && ($btn3.textContent === player2Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn1,
-            btn2: $btn2,
-            btn3: $btn3,
-            jugador: 2
-        });
-
-    } else if (($btn4.textContent === player2Move) && ($btn5.textContent === player2Move) && ($btn6.textContent === player2Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn4,
-            btn2: $btn5,
-            btn3: $btn6,
-            jugador: 2
-        });
-
-    } else if (($btn7.textContent === player2Move) && ($btn8.textContent === player2Move) && ($btn9.textContent === player2Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn7,
-            btn2: $btn8,
-            btn3: $btn9,
-            jugador: 2
-        });
-
-        // Verificar en cada columna si existen 3 'X' consecutivos
-    } else if (($btn1.textContent === player2Move) && ($btn4.textContent === player2Move) && ($btn7.textContent === player2Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn1,
-            btn2: $btn4,
-            btn3: $btn7,
-            jugador: 2
-        });
-
-    } else if (($btn2.textContent === player2Move) && ($btn5.textContent === player2Move) && ($btn8.textContent === player2Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn2,
-            btn2: $btn5,
-            btn3: $btn8,
-            jugador: 2
-        });
-
-    } else if (($btn3.textContent === player2Move) && ($btn6.textContent === player2Move) && ($btn9.textContent === player2Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn3,
-            btn2: $btn6,
-            btn3: $btn9,
-            jugador: 2
+    } else if ((botones.three.textContent === playerMove) && (botones.six.textContent === playerMove) && (botones.nine.textContent === playerMove)) {
+        message({
+            boton,
+            btn1: botones.three,
+            btn2: botones.six,
+            btn3: botones.nine,
+            jugador: playerTurn
         });
 
         // Verificar en la diagonal si existen 3 'X' consecutivos
-    } else if (($btn1.textContent === player2Move) && ($btn5.textContent === player2Move) && ($btn9.textContent === player2Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn1,
-            btn2: $btn5,
-            btn3: $btn9,
-            jugador: 2
+    } else if ((botones.one.textContent === playerMove) && (botones.five.textContent === playerMove) && (botones.nine.textContent === playerMove)) {
+        message({
+            boton,
+            btn1: botones.one,
+            btn2: botones.five,
+            btn3: botones.nine,
+            jugador: playerTurn
         });
 
-    } else if (($btn3.textContent === player2Move) && ($btn5.textContent === player2Move) && ($btn7.textContent === player2Move)) {
-        winner({
-            boton: $boton,
-            btn1: $btn3,
-            btn2: $btn5,
-            btn3: $btn7,
-            jugador: 2
+    } else if ((botones.three.textContent === playerMove) && (botones.five.textContent === playerMove) && (botones.seven.textContent === playerMove)) {
+        message({
+            boton,
+            btn1: botones.three,
+            btn2: botones.five,
+            btn3: botones.seven,
+            jugador: playerTurn
         });
+    }
+}
 
-
-    } else if (($btn1.textContent === player1Move || $btn1.textContent === player2Move) && ($btn2.textContent === player1Move || $btn2.textContent === player2Move) && ($btn3.textContent === player1Move || $btn3.textContent === player2Move) && ($btn4.textContent === player1Move || $btn4.textContent === player2Move) && ($btn5.textContent === player1Move || $btn5.textContent === player2Move) && ($btn6.textContent === player1Move || $btn6.textContent === player2Move) && ($btn7.textContent === player1Move || $btn7.textContent === player2Move) && ($btn8.textContent === player1Move || $btn8.textContent === player2Move) && ($btn9.textContent === player1Move || $btn9.textContent === player2Move)) {
+function tie({ botones, boton, jugador1, jugador2 }) {
+    if ((botones.one.textContent === jugador1 || botones.one.textContent === jugador2) && (botones.two.textContent === jugador1 || botones.two.textContent === jugador2) && (botones.three.textContent === jugador1 || botones.three.textContent === jugador2) && (botones.four.textContent === jugador1 || botones.four.textContent === jugador2) && (botones.five.textContent === jugador1 || botones.five.textContent === jugador2) && (botones.six.textContent === jugador1 || botones.six.textContent === jugador2) && (botones.seven.textContent === jugador1 || botones.seven.textContent === jugador2) && (botones.eigth.textContent === jugador1 || botones.eigth.textContent === jugador2) && (botones.nine.textContent === jugador1 || botones.nine.textContent === jugador2)) {
         Swal.fire({
             title: '<strong style="font-family: Kalam, cursive; color: #C3FF99;">NO hay un ganador</strong>',
             html: '<span style="font-family: Kalam, cursive;">¡Han quedado en empate!</span>',
@@ -285,7 +199,7 @@ function checkWinner() {
             background: '#16213E',
             showConfirmButton: true,
         });
-        let copyButtons = Array.from($boton);
+        let copyButtons = Array.from(boton);
         for (let i = 0; i < copyButtons.length; i++) {
             copyButtons[i].disabled = true;
             copyButtons[i].style.background = '#16213E';
@@ -296,6 +210,48 @@ function checkWinner() {
         $turn.textContent = '';
 
     }
+
+}
+function checkWinner() {
+    // llamamos a todos los botones creados
+    let buttons = {
+        'one': document.getElementById('1'),
+        'two': document.getElementById('2'),
+        'three': document.getElementById('3'),
+        'four': document.getElementById('4'),
+        'five': document.getElementById('5'),
+        'six': document.getElementById('6'),
+        'seven': document.getElementById('7'),
+        'eigth': document.getElementById('8'),
+        'nine': document.getElementById('9'),
+    }
+
+    const $boton = document.getElementsByClassName('boton');
+
+    /** ### X ### */
+    winner({
+        botones: buttons,
+        boton: $boton,
+        playerMove: player1Move,
+        playerTurn: player1
+    });
+
+    /** ### O ### */
+    winner({
+        botones: buttons,
+        boton: $boton,
+        playerMove: player2Move,
+        playerTurn: player2
+    });
+
+    /** ### TIE ### */
+    tie({
+        botones: buttons,
+        boton: $boton,
+        jugador1: player1Move,
+        jugador2: player2Move
+    });
+
 }
 
 function incio() {
